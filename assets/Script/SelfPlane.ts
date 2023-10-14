781e599ddb990e89f1173bbb58b6c5b6793642c8
Resuragam <1791614500@qq.com>
2023-10-14 23:14:23 +0800

    feat: airplane move

---
+++ assets/Script/SelfPlane.ts
@@ -0,0 +1,20 @@
+import { _decorator, Component, EventTouch, Input, input, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('SelfPlane')
+export class SelfPlane extends Component {
+    @property
+    public speed = 1;
+
+    start() {
+        input.on(Input.EventType.TOUCH_MOVE, this._touchMove, this);
+    }
+
+    update(deltaTime: number) {}
+
+    private _touchMove(touch: EventTouch) {
+        const delta = touch.getDelta();
+        let pos = this.node.position;
+        this.node.setPosition(pos.x + 0.075 * this.speed * delta.x, pos.y, pos.z - 0.075 * this.speed * delta.y);
+    }
+}
